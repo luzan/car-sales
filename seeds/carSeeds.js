d1d55b291098a8543d0554a06b33beeb0bdaf8c8
Luzan Baral <luzanbaral@gmail.com>
2022-06-15 17:23:39 -0500

    updated seed for validation

--- seeds/carSeeds.js
+++ seeds/carSeeds.js
@@ -16,47 +16,51 @@ mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true})
 const carsSeed = [
     {
         title: 'Used 2021 Tesla Model Y Long Range',
-        status: 'used',
-        year: '2021',
+        status: 'Used',
+        year: 2021,
         make: 'Tesla',
         distance: 13954,
         model: 'Model Y',
         price: 71995,
         bodyStyle: 'Sedan',
-        imageUrl: ['https://media.autoexpress.co.uk/image/private/s--X-WVjvBW--/f_auto,t_content-image-full-desktop@1/v1634205604/autoexpress/2021/10/Tesla%20Model%20Y%202021-4.jpg']
+        imageUrl: 'https://media.autoexpress.co.uk/image/private/s--X-WVjvBW--/f_auto,t_content-image-full-desktop@1/v1634205604/autoexpress/2021/10/Tesla%20Model%20Y%202021-4.jpg',
+        sellersNote: 'Great dependable vehicle with all the wanted accessories! Leather interior with heated front seats, power driver and passenger seats, dual moonroof, premium sound system, back-up camera, luggage rack, power lift gate and so much more.'
     },
     {
-        title: 'Used 2020 Ford EcoSport SE',
-        status: 'used',
-        year: '2020',
-        make: 'Ford',
+        title: 'Used 2020 Audi All road',
+        status: 'Used',
+        year: 2020,
+        make: 'Audi',
         distance: 12397,
-        model: 'EcoSport',
+        model: 'All road',
         price: 23899,
         bodyStyle: 'SUV',
-        imageUrl: ['https://www.cnet.com/a/img/resize/1f28dede9d7c2239607463537db1fc1feba51814/hub/2018/04/27/212e1c38-d0a2-41e8-a724-dc878a30fa8c/ogi1-001-2018-ford-ecosport-review.jpg?auto=webp&fit=crop&height=675&width=1200']
+        imageUrl: 'https://www.quattrodaily.com/wp-content/uploads/2020/07/C5-Audi-Allroad-1-1360x1008.jpg',
+        sellersNote: 'Great dependable vehicle with all the wanted accessories! Leather interior with heated front seats, power driver and passenger seats, dual moonroof, premium sound system, back-up camera, luggage rack, power lift gate and so much more.'
     },
     {
-        title: 'Used 2020 BMW X3 M Base (A8)',
-        status: 'used',
-        year: '2020',
+        title: 'Used 2020 BMW Z4 M Base (A8)',
+        status: 'Used',
+        year: 2020,
         make: 'BMW',
         distance: 11663,
-        model: 'X3 M',
+        model: 'Z4 M',
         price: 67832,
-        bodyStyle: 'SUV',
-        imageUrl: ['https://s3.us-east-2.amazonaws.com/dealer-inspire-vps-vehicle-images/110007841/5UXTY9C07L9B89978/bb6cb76d2682d6f5e5f6619ec51063cb.jpg']
+        bodyStyle: 'Coupe',
+        imageUrl: 'https://hips.hearstapps.com/hmg-prod/images/2020-bmw-z4-m40i-105-1568895913.jpg?crop=0.606xw:0.683xh;0.170xw,0.252xh&resize=640:*',
+        sellersNote: 'Great dependable vehicle with all the wanted accessories! Leather interior with heated front seats, power driver and passenger seats, dual moonroof, premium sound system, back-up camera, luggage rack, power lift gate and so much more.'
     },
     {
         title: '2022 Audi e-tron Premium Plus',
-        status: 'new',
-        year: '2022',
+        status: 'New',
+        year: 2022,
         make: 'Audi',
         distance: 1,
-        model: 'e-tron',
+        model: 'E-tron',
         price: 80630,
         bodyStyle: 'SUV',
-        imageUrl: ['https://www.autotrader.com/wp-content/uploads/2021/05/2021-audi-e-tron-premium-front-left-side-e1639075042759.jpg']
+        imageUrl: 'https://www.autotrader.com/wp-content/uploads/2021/05/2021-audi-e-tron-premium-front-left-side-e1639075042759.jpg',
+        sellersNote: 'Great dependable vehicle with all the wanted accessories! Leather interior with heated front seats, power driver and passenger seats, dual moonroof, premium sound system, back-up camera, luggage rack, power lift gate and so much more.'
     },
 
 ]
