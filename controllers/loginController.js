598a4321b858cbefaeb924926b3ecea3454cb675
Luzan Baral <luzanbaral@gmail.com>
2022-06-16 19:03:01 -0500

    update login controller

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,13 +1,44 @@
+const UserModel = require("../models/userSchema")
 
 const loginIndex = (req, res) => {
-    res.send('Login Index');
+    res.render('index');
 }
 
-const loginPost = (req, res) => {
-    console.log(req.body);
-    res.send('login post')
+const registerIndex = (req, res) => {
+    res.render('register');
+}
+
+const registerPost = async (req, res) => {
+    const { username, password } = req.body;
+    const user = new UserModel({
+        username: username,
+        password: password
+    });
+    await user.save();
+    res.redirect('/');
 }
 
 module.exports = {
-    loginIndex
+    loginIndex,
+    loginUser: function (username, password, callback) {
+        UserModel.findOne({ username: username }).exec(function (error, user) {
+            if (error) {
+                callback({ error: true })
+            } else if (!user) {
+                callback({ error: true })
+            } else {
+                user.comparePassword(password, function (matchError, isMatch) {
+                    if (matchError) {
+                        callback({ error: true })
+                    } else if (!isMatch) {
+                        callback({ error: true })
+                    } else {
+                        callback({ success: true })
+                    }
+                })
+            }
+        })
+    },
+    registerIndex,
+    registerPost
 }
